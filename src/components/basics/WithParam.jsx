d5370c842eac0144fd637771ceccf8fd6f2038e2
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-09-14 08:20:00 -0300

    add logic to component

--- src/components/basics/WithParam.jsx
+++ src/components/basics/WithParam.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 export default function WithParam(props) {
+  const status = props.grade >= 7 ? 'Approved' : 'Recovering'
+
   return (
     <div>
       <h2>{props.title}</h2>
-      <h3>Student {props.student} has grade {props.grade}</h3>
+      <p>Student
+        <strong> {props.student}</strong>  has grade
+        <strong> {props.grade}</strong> and is
+        <strong> {status}</strong>!</p>
     </div>
   )
 }
