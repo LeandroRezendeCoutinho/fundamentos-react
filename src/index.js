ed8e337487329f7bb18ba6068dfa942cef00bd23
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-09-14 07:59:30 -0300

    component with param/props

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import First from "./components/basics/First";
+import WithParam from "./components/basics/WithParam";
 
 ReactDOM.render(
-  <div>
+  <div id="app">
     <First></First>
+    <WithParam
+      title="Second component"
+      subtitle="Very cool">
+    </WithParam>
   </div>,
   document.getElementById('root')
 )
