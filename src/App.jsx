596e205d12ccbb69d6f79d0693ce97834cfe6459
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-09-15 08:51:53 -0300

    create card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import First from "./components/basics/First";
 import WithParam from "./components/basics/WithParam";
 import Fragment from "./components/basics/Fragment";
 import Random from "./components/basics/Random";
+import Card from "./components/layout/Card";
 
 export default () =>
   <div id="app">
+    <h1>React fundamentals</h1>
+
+    <Card title="Card example"></Card>
+
     <Random min={10} max={20} />
     <Fragment />
     <WithParam
