d3511f2cce3b7b28ba0873a094150e82ce1d53d0
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-09-16 08:49:03 -0300

    add flex-box layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,33 @@ import WithParam from "./components/basics/WithParam";
 import Fragment from "./components/basics/Fragment";
 import Random from "./components/basics/Random";
 import Card from "./components/layout/Card";
+import "./App.css";
 
 export default () =>
-  <div id="app">
+  <div className="App">
     <h1>React fundamentals</h1>
 
-    <Card title="Random example">
-      <Random min={10} max={20} />
-    </Card>
+    <div className="Cards">
+      <Card title="Random example">
+        <Random min={10} max={20} />
+      </Card>
 
-    <Card title="Fragment example">
-      <Fragment />
-    </Card>
+      <Card title="Fragment example">
+        <Fragment />
+      </Card>
 
-    <Card title="Second example">
-      <WithParam
-        title="Second component"
-        student="Xavier"
-        grade={9.4}>
-      </WithParam>
-    </Card>
+      <Card title="Second example">
+        <WithParam
+          title="Second component"
+          student="Xavier"
+          grade={9.4}>
+        </WithParam>
+      </Card>
 
-    <Card title="First example">
-      <First></First>
-    </Card>
+      <Card title="First example">
+        <First></First>
+      </Card>
+
+    </div>
   </div>
 
